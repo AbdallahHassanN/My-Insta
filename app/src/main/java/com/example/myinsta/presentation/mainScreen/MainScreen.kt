f453e19f7d1c7d3216ef8595d4b0c43c3c55c130
AbdallahHassanN <abdohassan2000@hotmail.com>
2024-02-11 15:47:50 +0200

    add SnackBar

--- app/src/main/java/com/example/myinsta/presentation/mainScreen/MainScreen.kt
+++ app/src/main/java/com/example/myinsta/presentation/mainScreen/MainScreen.kt
@@ -2,20 +2,22 @@ package com.example.myinsta.presentation.mainScreen
 
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.util.Log
-import android.widget.Toast
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,9 +27,7 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.myinsta.R
-import com.example.myinsta.common.Constants
 import com.example.myinsta.common.Constants.TAG
-import com.example.myinsta.common.showToastMessage
 import com.example.myinsta.components.CenteredText
 import com.example.myinsta.components.LogoImage
 import com.example.myinsta.components.MyButton
@@ -44,20 +44,28 @@ fun MainScreen(navController: NavController) {
     val emailValue by viewModel.email.collectAsStateWithLifecycle()
     val passwordValue by viewModel.password.collectAsStateWithLifecycle()
     val signInValue = viewModel.signInState.collectAsState()
-    val context = LocalContext.current
+    val snackBarHostState = remember { SnackbarHostState() }
+
+    // Function to show the snackBar
+    suspend fun showSnackBar(message: String) {
+        snackBarHostState.showSnackbar(message)
+    }
 
     signInValue.value.let {
         when (it) {
             is Resource.Error -> {
                 LaunchedEffect(Unit) {
-                    showToastMessage(context, "Invalid email or password")
+                    val error = it.message
+                    showSnackBar(error.toString())
                 }
             }
+
             is Resource.Loading -> {
                 LaunchedEffect(Unit) {
                     Log.d(TAG, "Loading")
                 }
             }
+
             is Resource.Success -> {
                 LaunchedEffect(Unit) {
                     navController.navigate(Screens.FeedScreen.route) {
@@ -110,6 +118,21 @@ fun MainScreen(navController: NavController) {
                 navController.navigate(Screens.RegisterScreen.route)
             }, text = "Have an account ?", link = " Register Here"
         )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 16.dp), // Adjust padding as needed
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            SnackbarHost(
+                hostState = snackBarHostState,
+            ) { snackBar ->
+                Snackbar(
+                    snackbarData = snackBar,
+                    containerColor = Color.Red
+                )
+            }
+        }
     }
 }
 
--- app/src/main/java/com/example/myinsta/presentation/registerScreen/RegisterScreen.kt
+++ app/src/main/java/com/example/myinsta/presentation/registerScreen/RegisterScreen.kt
@@ -1,16 +1,21 @@
 package com.example.myinsta.presentation.registerScreen
 
 import android.util.Log
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,7 +26,6 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.myinsta.R
 import com.example.myinsta.common.Constants
-import com.example.myinsta.common.showToastMessage
 import com.example.myinsta.components.CenteredText
 import com.example.myinsta.components.LogoImage
 import com.example.myinsta.components.MyButton
@@ -39,22 +43,28 @@ fun RegisterScreen(navController: NavController) {
     val usernameValue by viewModel.username.collectAsStateWithLifecycle()
     val passwordValue by viewModel.password.collectAsStateWithLifecycle()
     val registerState = viewModel.registerState.collectAsState()
-    val context = LocalContext.current
+    val snackBarHostState = remember { SnackbarHostState() }
 
+    // Function to show the snackBar
+    suspend fun showSnackBar(message: String) {
+        snackBarHostState.showSnackbar(message)
+    }
 
     registerState.value.let {
         when (it) {
             is Resource.Error -> {
                 LaunchedEffect(Unit) {
                     val error = it.message
-                    showToastMessage(context,error.toString())
+                    showSnackBar(error.toString())
                 }
             }
+
             is Resource.Loading -> {
                 LaunchedEffect(Unit) {
                     Log.d(Constants.TAG, "Loading")
                 }
             }
+
             is Resource.Success -> {
                 LaunchedEffect(Unit) {
                     navController.navigate(Screens.FeedScreen.route) {
@@ -85,13 +95,13 @@ fun RegisterScreen(navController: NavController) {
         UsernameOrEmailTextField(
             label = "Enter Full Name",
             inputValue = fullNameValue,
-            onQueryChanged = {viewModel.onFullNameChanged(it)},
+            onQueryChanged = { viewModel.onFullNameChanged(it) },
             isError = viewModel.fullNameValidation.value?.isSuccess?.not() ?: false,
         )
         UsernameOrEmailTextField(
             label = "Enter Username",
             inputValue = usernameValue,
-            onQueryChanged = {viewModel.onUsernameChanged(it)},
+            onQueryChanged = { viewModel.onUsernameChanged(it) },
             isError = viewModel.usernameValidation.value?.isSuccess?.not() ?: false,
         )
         PasswordTextField(
@@ -119,6 +129,21 @@ fun RegisterScreen(navController: NavController) {
             text = "Have an account ?",
             link = " Log in"
         )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 16.dp), // Adjust padding as needed
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            SnackbarHost(
+                hostState = snackBarHostState,
+            ) { snackBar ->
+                Snackbar(
+                    snackbarData = snackBar,
+                    containerColor = Color.Red
+                )
+            }
+        }
     }
 }
 
